492fc529ee1a3a26671b4298f0175f395fdaefa0
Dragon·Dylan <1984856762@qq.com>
2021-07-06 22:49:46 +0800

    Learning C by myself
    修改保存内容

--- 代码：计算长方体面积.c
+++ 代码：计算长方体面积.c
@@ -18,7 +18,7 @@ int main()
 	printf("高：\n");
 	scanf("%d", &Height);
 
-
+	//在C语言中scanf是一种不安全的函数，所以要在代码开头输入#define _CRT_SECURE_NO_WARNINGS 1以忽略不安全的警告从而才能运行此代码
 
 	result = calculate(Long, Width, Height);
 
